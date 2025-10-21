7c24134e3ed80abeb1741b0e95537f6e3158ee3b
RaveHunter05 <paulsotelor@yahoo.es>
2025-10-21 02:05:01 -0600

    feat: add tailwind to react native project

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,52 +1,17 @@
-import { ScrollView, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/hello-wave';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function HomeScreen() {
     return (
-        <ScrollView contentContainerStyle={styles.mainContainer}>
-            <ThemedView style={styles.titleContainer}>
-                <ThemedText type="title">Welcome!</ThemedText>
-                <HelloWave />
-            </ThemedView>
-            <ThemedView style={styles.stepContainer}>
-                <ThemedText type="subtitle">
-                    Easy Payments With Criptos Using PYUSD
-                </ThemedText>
-                <ThemedText>
-                    Send and receive payments with Crypto Using PYUSD
-                </ThemedText>
-            </ThemedView>
-        </ScrollView>
+        <View className="flex-1 items-center justify-center bg-white text-center">
+            <Text className="text-2xl font-bold text-blue-400">
+                Welcome to EasyCripto! 🖐️
+            </Text>
+            <Text className="text-sm font-bold text-center mt-4">
+                C2C and B2C Payments
+            </Text>
+            <Text className="text-sm font-bold text-center">
+                with PYUSD made Easy
+            </Text>
+        </View>
     );
 }
-
-const styles = StyleSheet.create({
-    mainContainer: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-	backgroundColor: "#fff",
-	height: '100%',
-	padding: 40
-    },
-    titleContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-	color: "blue",
-        gap: 8,
-    },
-    stepContainer: {
-        gap: 8,
-        marginBottom: 8,
-    },
-    reactLogo: {
-        height: 178,
-        width: 290,
-        bottom: 0,
-        left: 0,
-        position: 'absolute',
-    },
-});
